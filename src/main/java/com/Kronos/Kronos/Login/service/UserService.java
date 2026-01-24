edba37504bdabf1a1d487d55cc425c7b8e1498da
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-01-24 16:59:13 -0300

    user service created

---
+++ src/main/java/com/Kronos/Kronos/Login/service/UserService.java
@@ -0,0 +1,46 @@
+package com.Kronos.Kronos.Login.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.Kronos.Kronos.Login.model.User;
+import com.Kronos.Kronos.Login.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public Optional<Object> updateUser(Long id, User userDetails) {
+        return userRepository.findById(id)
+        .map(user -> {
+            user.setUsername(userDetails.getUsername());
+            user.setPassword(userDetails.getPassword());
+            return userRepository.save(user);
+        });
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}

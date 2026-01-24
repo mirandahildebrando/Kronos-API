3c8fd59688dfc326ccd0b0fafc0bbdfe312fbc1d
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-01-24 16:28:18 -0300

    first commit

---
+++ src/main/java/com/Kronos/Kronos/KronosApplication.java
@@ -0,0 +1,13 @@
+package com.Kronos.Kronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KronosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KronosApplication.class, args);
+	}
+
+}

c15e821ae78a382114b908e0d1b35418fb30adab
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-01-30 13:37:04 -0300

    creating the structure for the product entity

---
+++ src/main/java/com/Kronos/Kronos/Product/model/Product.java
@@ -0,0 +1,27 @@
+package com.Kronos.Kronos.Product.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+}

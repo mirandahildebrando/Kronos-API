ecbccbdba66e8bd464087653cdc926d463714c7e
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-01-30 21:17:26 -0300

    POS repository layer created

---
+++ src/main/java/com/Kronos/Kronos/PDV/repository/SaleRepository.java
@@ -0,0 +1,11 @@
+package com.Kronos.Kronos.PDV.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Kronos.Kronos.PDV.model.Sale;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}

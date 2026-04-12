4eb033ed0ed9f90391d906c98331b4414ab774bc
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-12 19:04:50 -0300

    Adding information to the database so that when the system starts, it already has something to display

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
+-- Login
 INSERT INTO users (username, password) VALUES ('admin', '12345678');
 INSERT INTO users (username, password) VALUES ('teste', '12345678');
+
+-- PDV
+INSERT INTO pdv (sale) VALUES ('PDV 1');

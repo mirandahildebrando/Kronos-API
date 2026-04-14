176b156e7c0295de42894f44e78fdbb33de0e971
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-14 17:23:10 -0300

    Replacing the h2 database with PostgreSQL, tested and working API with database on SupaBase

--- src/main/java/com/Kronos/Kronos/PDV/service/SaleService.java
+++ src/main/java/com/Kronos/Kronos/PDV/service/SaleService.java
@@ -2,7 +2,6 @@ package com.Kronos.Kronos.PDV.service;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 

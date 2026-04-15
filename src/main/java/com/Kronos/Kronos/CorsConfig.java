ba6a481e1dea392b98aa7aa4e7603ba8b2e64ea7
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-15 13:30:59 -0300

    enable global cors

--- src/main/java/com/Kronos/Kronos/CorsConfig.java
+++ src/main/java/com/Kronos/Kronos/CorsConfig.java
@@ -2,8 +2,7 @@ package com.Kronos.Kronos;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
@@ -13,9 +12,10 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };

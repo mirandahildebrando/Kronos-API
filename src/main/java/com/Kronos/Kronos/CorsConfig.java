bbbf2b59d2749c967f9b988020319ca943d98694
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-15 14:19:03 -0300

    update

--- src/main/java/com/Kronos/Kronos/CorsConfig.java
+++ src/main/java/com/Kronos/Kronos/CorsConfig.java
@@ -8,16 +8,15 @@ import org.springframework.web.servlet.config.annotation.*;
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
-    }
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*");
+        }
+    };
+  }
 }

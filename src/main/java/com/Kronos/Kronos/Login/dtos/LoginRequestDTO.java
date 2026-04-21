e05062e4266593d4906bdd0bff85b87226fda9ca
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-21 18:14:03 -0300

    Changing LoginRequestDTO to test bug

--- src/main/java/com/Kronos/Kronos/Login/dtos/LoginRequestDTO.java
+++ src/main/java/com/Kronos/Kronos/Login/dtos/LoginRequestDTO.java
@@ -1,9 +1,13 @@
 package com.Kronos.Kronos.Login.dtos;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record LoginRequestDTO(
     @JsonProperty("username") String username,
     @JsonProperty("password") String password
-) {
+)
+{ 
+        @JsonCreator
+        public LoginRequestDTO {}
 }

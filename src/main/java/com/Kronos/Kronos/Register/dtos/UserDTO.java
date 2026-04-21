799085078c13c02ee7504c3d4bad5d3a25e2ca1f
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-21 18:51:39 -0300

    Removing the validations to test again

--- src/main/java/com/Kronos/Kronos/Register/dtos/UserDTO.java
+++ src/main/java/com/Kronos/Kronos/Register/dtos/UserDTO.java
@@ -4,11 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserDTO(
-    @NotBlank 
-    @Size(min = 5, max = 30) 
-    String username,
-
-    @NotBlank 
-    @Size(min = 8, max = 20) 
+    String username, 
     String password
 ) {}

87110dc1b4d9557f9abc1d0c06de2defeb773193
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-04-22 16:08:21 -0300

    update service Login

--- src/main/java/com/Kronos/Kronos/Login/service/LoginService.java
+++ src/main/java/com/Kronos/Kronos/Login/service/LoginService.java
@@ -1,12 +1,9 @@
 package com.Kronos.Kronos.Login.service;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.Kronos.Kronos.Login.dtos.LoginRequestDTO;
-import com.Kronos.Kronos.Login.model.Login;
-import com.Kronos.Kronos.Login.repository.LoginRepository;
+import com.Kronos.Kronos.Register.model.User;
 import com.Kronos.Kronos.Register.repository.UserRepository;
 
 @Service
@@ -21,16 +18,12 @@ public class LoginService {
     public String login(LoginRequestDTO dto) {
 
         User user = userRepository.findByUsername(dto.getUsername())
-            .orElseThrow(() -> 
-                new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado")
-            );
+            .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 
         if (!user.getPassword().equals(dto.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Senha inválida");
+            throw new RuntimeException("Senha inválida");
         }
 
         return "Login realizado com sucesso!";
     }
-
-
 }
